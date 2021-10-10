1df5cc1d4efa7fbb9116756e9b61920eff530326
Martin Gonzalez <marttingonzalez@gmail.com>
2021-10-10 19:27:43 -0300

    se crean ListOfPhotoCard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Category } from './componets/Category';
 import { ListOfCategories } from './componets/ListOfCategories';
 import { GlobalStyle } from './GobalStyles';
-import { PhotoCard } from './componets/PhotoCard';
+import { ListOfPhotoCards } from './componets/ListOfPhotoCards';
 
 export const App = () => (
   <>
     <GlobalStyle />
     <ListOfCategories />
-    <PhotoCard />
+    <ListOfPhotoCards />
   </>
 );
