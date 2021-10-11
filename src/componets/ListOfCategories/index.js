15d6df3d33a0fc2e11b97a5b0b307a4d8f3ddf0a
Martin Gonzalez <marttingonzalez@gmail.com>
2021-10-11 14:02:34 -0300

    se agregan hooks para llamada a API backend

--- src/componets/ListOfCategories/index.js
+++ src/componets/ListOfCategories/index.js
@@ -1,26 +1,41 @@
-import React from "react";
-import { Category } from "../Category";
-import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json"
-
+import React, { useEffect, useState } from 'react';
+import { Category } from '../Category';
+import { List, Item } from './styles';
 
 export const ListOfCategories = () => {
-    return (
-        <List>
-            {
-                categories.map(category => 
-                <Item key={category.id}>
-                    <Category
-                  /* cover = {category.cover} 
-                     path = {category.path}
-                     emoji = {category.emoji} 
-                     Podemos hacer eso o 
-                     usar el spread operator */       
-                    {...category}
-                    />
-                </Item>
-                )
-            }
-        </List>
-    )
-}
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function () {
+    window
+      .fetch('https://petgram-server-martintomaco.vercel.app/categories')
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response);
+      });
+  }, []);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category
+            /* cover = {category.cover} 
+                 path = {category.path}
+                 emoji = {category.emoji} 
+                 Podemos hacer eso o 
+                 usar el spread operator */
+            {...category}
+          />
+        </Item>
+      ))}
+    </List>
+  );
+
+  return (
+    <>
+    {renderList()}
+    {renderList(true)}
+    </>
+  )
+  
+};
--- src/componets/ListOfCategories/styles.js
+++ src/componets/ListOfCategories/styles.js
@@ -10,6 +10,22 @@ export const List = styled.ul`
 
   padding-inline-start: 0;
   margin-bottom: 10px;
+
+  &.fixed {
+    position: fixed;
+    top: -20px;
+    right:0;
+    left: 0;
+    margin: 0 auto;
+    padding: 5px;
+    max-width: 400px;
+    border-radius: 60px;
+    background: #fff;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    transform: scale(.6);
+    z-index: 1;
+  }
+
 `
 
 export const Item = styled.li`
