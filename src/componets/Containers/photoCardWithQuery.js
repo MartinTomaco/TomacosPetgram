ae97c7f09ce74d86c4c553c77cff273f2b731dab
Martin Gonzalez <marttingonzalez@gmail.com>
2021-10-20 12:37:15 -0300

    planceholder durante loading

--- src/componets/Containers/photoCardWithQuery.js
+++ src/componets/Containers/photoCardWithQuery.js
@@ -1,18 +1,44 @@
-import React from 'react'
-import { PhotoCard } from '../PhotoCard'
-import { useGetSinglePhoto } from '../../hooks/useGetSinglePhoto'
+import React from 'react';
+import ReactPlaceholder from 'react-placeholder';
+import {
+  TextBlock,
+  RectShape,
+  RoundShape,
+} from 'react-placeholder/lib/placeholders';
+import { PhotoCard } from '../PhotoCard';
+import { useGetSinglePhoto } from '../../hooks/useGetSinglePhoto';
 
 export const PhotoCardWithQuery = ({ id }) => {
-  const { loading, error, data } = useGetSinglePhoto(id)
+  const { loading, error, data } = useGetSinglePhoto(id);
+
+  const photoCardSkeleton = (
+    <React.Fragment>
+      <RectShape color="#eee" style={{ height: '281px', marginBottom: 10 }} />
+      <div
+        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+      >
+        <RoundShape
+          color="#eee"
+          style={{ width: 50, height: 50, marginLeft: 10, marginRight: 10 }}
+        />
+        <TextBlock color="#eee" rows={1} style={{ width: 120 }} />
+      </div>
+    </React.Fragment>
+  );
 
   if (error) {
     return <p>Internal Server Error: {error.message}</p>;
   }
-  if (loading) {
-    return <p>Loading...</p>;
-  }
 
-  const { photo = {} } = data
+  const photo = data || {}; //Si aún data es undefined le asigno un objeto vacio
 
-  return <PhotoCard {...photo} />
-}
+  return (
+    <ReactPlaceholder
+      ready={!loading}
+      showLoadingAnimation={true}
+      customPlaceholder={photoCardSkeleton}
+    >
+      <PhotoCard loading={loading} {...photo} />
+    </ReactPlaceholder>
+  );
+};
--- src/componets/ListOfPhotoCards/index.js
+++ src/componets/ListOfPhotoCards/index.js
@@ -1,28 +1,51 @@
 import React from 'react';
-import { useGetPhotos } from '../../hooks/useGetPhotos';
+import ReactPlaceholder from 'react-placeholder';
+import {
+  TextBlock,
+  RectShape,
+  RoundShape,
+} from 'react-placeholder/lib/placeholders';
 
+import { useGetPhotos } from '../../hooks/useGetPhotos';
 import { PhotoCard } from '../PhotoCard';
 import { List } from './styles';
 
-
 export const ListOfPhotoCards = ({ categoryId }) => {
-
+  const photoCardSkeleton = (
+    <React.Fragment>
+      <RectShape color="#eee" style={{ height: '281px', marginBottom: 10 }} />
+      <div
+        style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+      >
+        <RoundShape
+          color="#eee"
+          style={{ width: 50, height: 50, marginLeft: 10, marginRight: 10 }}
+        />
+        <TextBlock color="#eee" rows={1} style={{ width: 120 }} />
+      </div>
+    </React.Fragment>
+  );
 
   const { loading, error, data } = useGetPhotos(categoryId);
-  
+
   if (error) {
     return <p>Internal Server Error: {error.message}</p>;
   }
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+
+
+  const photoData = data || {photos:[]} //Si aún no inicializo data le paso un objeto con la propiedad photos con arreglo vacio
 
   return (
-    <List>
-      {data.photos.map((photo) => (
-        <PhotoCard key={photo.id} {...photo} />
-      ))}
-    </List>
+    <ReactPlaceholder
+      ready={!loading}
+      showLoadingAnimation={true}
+      customPlaceholder={photoCardSkeleton}
+    >
+      <List>
+        {photoData.photos.map((photo) => (
+          <PhotoCard key={photo.id} {...photo} />
+        ))}
+      </List>
+    </ReactPlaceholder>
   );
-
 };
