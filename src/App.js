c486fd6d16349dbb68fd6a86907da1d0d1310653
Martin Gonzalez <marttingonzalez@gmail.com>
2021-11-03 14:24:41 -0300

    agrega pages /favs y /user

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import { LogoSVG } from './componets/LogoSVG';
 import { NavBar } from './componets/NavBar';
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const isLogged = false
 
 export const App = () => {
-
   return (
     <>
+      <GlobalStyle />
       <BrowserRouter>
-        <GlobalStyle />
         <LogoSVG />
         <Switch>
           <Route path="/pet/:CategoryId" component={Home} />
           <Route path="/detail/:DetailId" component={Detail} />
+          <Route exact path='/favs' render={() => isLogged ? <Favs /> : <NotRegisteredUser />} />
+          <Route exact path='/user' render={() => isLogged ? <User /> : <NotRegisteredUser />} />
           <Route path="/" component={Home} />
         </Switch>
-        <NavBar/>
+        <NavBar />
       </BrowserRouter>
     </>
   );
