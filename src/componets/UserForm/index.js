da16cf4952078a561658d62d54832b046c3cb6bb
Martin Gonzalez <marttingonzalez@gmail.com>
2021-11-06 17:23:16 -0300

    registrar un user con graphql usando useMutetion

--- src/componets/UserForm/index.js
+++ src/componets/UserForm/index.js
@@ -1,37 +1,34 @@
 import React from 'react';
-import {useInputValue} from '../../hooks/useInitialState'
-import {Form, Input, Button, Title} from './styles'
+import { useInputValue } from '../../hooks/useInitialState';
+import { Form, Input, Button, Title } from './styles';
 
 export const UserForm = ({ title, onSubmit }) => {
- 
+  const email = useInputValue('');
+  const password = useInputValue('');
 
-  const email = useInputValue('')
-  const password = useInputValue('')
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ 
+      email: email.value, 
+      password: password.value 
+    });
+  };
 
   return (
     <>
-    <Title>{title}</Title>
-    <Form onSubmit={onSubmit}>
-    
-      {/* 
+      <Title>{title}</Title>
+      <Form onSubmit={handleSubmit}>
+        {/* 
         <input
         placeholder="Email"
         value={email.value}
         onChange={email.onChange}
         /> 
       */}
-      <Input
-        placeholder="Email"
-        {...email}
-      />
-      <Input
-        placeholder="Password"
-        type="password"
-        {...password}
-      />
-      <Button>{title}</Button>
-    </Form>
+        <Input placeholder="Email" {...email} />
+        <Input placeholder="Password" type="password" {...password} />
+        <Button>{title}</Button>
+      </Form>
     </>
   );
 };
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import Context from '../Context'
-import { UserForm } from '../componets/UserForm'
+import React from 'react';
+import Context from '../Context';
+import { UserForm } from '../componets/UserForm';
+import { useRegisterMutation } from '../hooks/useRegisterMutation';
 
-export const NotRegisteredUser = () => (
+export const NotRegisteredUser = () => {
+  const registerMutation = useRegisterMutation();
+  return (
     <Context.Consumer>
-        { 
-            ({isAuth, activateAuth})=> {
-                return(
-                    <>
-                    <UserForm 
-                    title={'Registrarse'}
-                    onSubmit={activateAuth}
-                    />
-                    <UserForm 
-                    title={'Iniciar sesion'}
-                    onSubmit={activateAuth}
-                    />
-                    </>
-                )
-            }
+      {
+        ({ activateAuth }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            registerMutation({ variables })
+              .then(activateAuth)
+          }
+
+          return (
+            <>
+              <UserForm title='Registrarse' onSubmit={onSubmit} />
+              <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+            </>
+          )
         }
+      }
     </Context.Consumer>
-)
+  )
+}
