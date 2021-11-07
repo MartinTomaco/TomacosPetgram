ccb5d44533896e581c1e23dcccd64015a728970b
Martin Gonzalez <marttingonzalez@gmail.com>
2021-11-07 20:50:40 -0300

    se refactoriza para usar useContext

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Context from '../Context';
+import React, { useContext } from 'react';
+import { Context } from '../Context';
 import { UserForm } from '../componets/UserForm';
 import { useRegisterMutation } from '../hooks/useRegisterMutation';
 import { useLoginMutation } from '../hooks/useLoginMutatio';
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context);
   const {
     registerMutation,
     data,
@@ -16,48 +17,44 @@ export const NotRegisteredUser = () => {
     loading: loadingLogin,
     error: errorLogin,
   } = useLoginMutation();
-  return (
-    <Context.Consumer>
-      {({ activateAuth }) => {
-        const onSubmitRegister = ({ email, password }) => {
-          const input = { email, password };
-          const variables = { input };
-          registerMutation({ variables }).then(({data}) => {
-            const { signup } = data
-            activateAuth(signup)
-          })
-        }
-        const onSubmitLogin = ({ email, password }) => {
-          const input = { email, password };
-          const variables = { input };
-          loginMutation({ variables }).then(({data}) => {
-            const {login} = data
-            activateAuth(login)
-          })
-        }
 
-        const errorMsg =
-          errorRegister && 'El usuario ya existe o hay algún problema.';
-        const errorLoginMsg = errorLogin && 'Usuario o contraseña incorrectos.';
-        return (
-          <>
-            <UserForm
-              disabled={loadingRegister}
-              error={errorMsg}
-              title="Registrarse"
-              onSubmit={onSubmitRegister}
-              loading={loadingRegister}
-            />
-            <UserForm
-              disabled={loadingLogin}
-              error={errorLoginMsg}
-              title="Iniciar Sesión"
-              onSubmit={onSubmitLogin}
-              loading={loadingLogin}
-            />
-          </>
-        );
-      }}
-    </Context.Consumer>
+  const onSubmitRegister = ({ email, password }) => {
+    const input = { email, password };
+    const variables = { input };
+    registerMutation({ variables }).then(({ data }) => {
+      const { signup } = data;
+      activateAuth(signup);
+    });
+  };
+  const onSubmitLogin = ({ email, password }) => {
+    const input = { email, password };
+    const variables = { input };
+    loginMutation({ variables }).then(({ data }) => {
+      const { login } = data;
+      activateAuth(login);
+    });
+  };
+
+  const errorMsg =
+    errorRegister && 'El usuario ya existe o hay algún problema.';
+  const errorLoginMsg = errorLogin && 'Usuario o contraseña incorrectos.';
+
+  return (
+    <>
+      <UserForm
+        disabled={loadingRegister}
+        error={errorMsg}
+        title="Registrarse"
+        onSubmit={onSubmitRegister}
+        loading={loadingRegister}
+      />
+      <UserForm
+        disabled={loadingLogin}
+        error={errorLoginMsg}
+        title="Iniciar Sesión"
+        onSubmit={onSubmitLogin}
+        loading={loadingLogin}
+      />
+    </>
   );
 };
