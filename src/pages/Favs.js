1d1d29c64bea669221a49617440ccbf24454500a
Martin Gonzalez <marttingonzalez@gmail.com>
2021-11-21 21:26:56 -0300

    Se agrega custom hook para crear fav de cada usuario

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ListOfFavs } from '../componets/ListOfFavs'
+import { useGetFavorites } from '../hooks/useGetFavorites'
 
 export const Favs = () => {
+    const { loading, error, data } = useGetFavorites();
+    
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error!</p>
+    const { favs } = data;
     return (
-        <h1>
-            Favs
-        </h1>
+        <ListOfFavs favs={favs}/>
     )
 }
